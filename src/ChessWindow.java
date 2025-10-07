881e59e2c84c1efe81efea5c4040f784ce7cd016
Noah Heckel <145144013+Bubbletea00@users.noreply.github.com>
2025-10-07 23:05:34 +0200

    added sprites

---
+++ src/ChessWindow.java
@@ -0,0 +1,102 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class ChessWindow {
+    int windowWidth = 1200;
+    int windowHeight = 800;
+
+    Color COLOR_DARK_SQUARE = new Color(181, 136, 99);
+    Color COLOR_LIGHT_SQUARE = new Color(240, 217, 181);
+    Color COLOR_BACKGROUND = new Color(40,40,40);
+
+    JFrame frame = new JFrame("Chess");
+    JPanel board = new JPanel();
+    JPanel rightPanel = new JPanel();
+
+    JButton[][] boardButtons = new JButton[8][8];
+
+    SpriteManager spriteManager = new SpriteManager();
+
+    public ChessWindow() {
+        initWindow();
+    }
+
+    private void initWindow() {
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setPreferredSize(new Dimension(windowWidth, windowHeight));
+        frame.getContentPane().setBackground(COLOR_BACKGROUND);
+        frame.setResizable(false);
+
+
+        board.setLayout(new GridLayout(8, 8));
+        board.setPreferredSize(new Dimension(800, 800));
+        createBoardSquares();
+        frame.add(board, BorderLayout.WEST);
+
+
+
+        rightPanel.setPreferredSize(new Dimension(400, 800));
+        rightPanel.setBackground(COLOR_BACKGROUND);
+        rightPanel.setLayout(new BorderLayout());
+
+        JLabel placeholder = new JLabel("Game History Area", SwingConstants.CENTER);
+        placeholder.setForeground(Color.LIGHT_GRAY);
+        rightPanel.add(placeholder, BorderLayout.CENTER);
+
+        frame.add(rightPanel, BorderLayout.CENTER);
+
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private void createBoardSquares() {
+        //TODO file and rank numbers and letters
+        for (int file = 0; file < 8; file++) {
+            for (int row = 0; row < 8; row++) {
+                JButton square = new JButton();
+                square.setOpaque(true);
+//                square.setContentAreaFilled(false);
+                square.setBorderPainted(false);
+//                square.setEnabled(false);
+                square.setFocusPainted(false);
+
+                if ((file + row) % 2 == 0)
+                    square.setBackground(COLOR_LIGHT_SQUARE); // light
+                else
+                    square.setBackground(COLOR_DARK_SQUARE);   // dark
+
+                boardButtons[file][row] = square;
+                board.add(square);
+            }
+        }
+        assignAllSprites(new Board());
+    }
+
+    private void assignAllSprites(Board board){
+        Pieces[][] layout = board.getBoard();
+
+        for (int file = 0; file < 8; file++) {
+            for (int rank = 0; rank < 8; rank++) {
+                Pieces piece = layout[rank][file];
+                JButton square = boardButtons[rank][file];
+
+                if (piece == null || piece == Pieces.EMPTY) {
+                    square.setIcon(null);
+                    continue;
+                }
+
+                String key = piece.name().toLowerCase();
+                ImageIcon icon = spriteManager.get(key);
+
+                square.setIcon(icon);
+
+            }
+        }
+
+
+    }
+}
---
+++ src/SpriteManager.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class SpriteManager {
+    private final Map<String, ImageIcon> sprites = new HashMap<>();
+
+    private int scaling = 64;
+    
+    public SpriteManager(){
+        loadSprites();
+    }
+
+    private void loadSprites() {
+        //todo replace Sting arrays with enums?
+        String[] colors = {"white", "black"};
+        String[] pieces = {"bishop", "king", "knight", "pawn", "queen", "rook"};
+
+        for(String color : colors){
+            for(String piece : pieces){
+                String name = color + "_" + piece;
+
+                System.out.println("name: " + "/assets/" + name + ".png");
+                System.out.println("getClass().getResource(\"/assets/\" + name + \".png\") = " + getClass().getResource("/assets/" + name + ".png"));
+
+                ImageIcon icon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/assets/" + name + ".png")));
+                Image scaled = icon.getImage().getScaledInstance(scaling, scaling, Image.SCALE_AREA_AVERAGING);
+                sprites.put(name, new ImageIcon(scaled));
+
+            }
+        }
+    }
+
+    public ImageIcon get(String name){
+        return sprites.get(name);
+    }
+
+    public int getScaling(){
+        return scaling;
+    }
+
+    public void setScaling(int scaling){
+        this.scaling = scaling;
+    }
+}
+
