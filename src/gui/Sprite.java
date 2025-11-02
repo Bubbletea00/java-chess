e028989a049b484174666cd4a13e2d480ee3685e
Noah Heckel <145144013+Bubbletea00@users.noreply.github.com>
2025-11-02 17:32:47 +0100

    Why The Fuck?

--- src/gui/Sprite.java
+++ src/gui/Sprite.java
@@ -5,18 +5,10 @@ import java.awt.image.BufferedImage;
 
 public class Sprite extends BufferedImage{
 
-    public Sprite(int width, int height, int imageType) {
-        super(width, height, imageType);
-    }
-
     public Sprite(BufferedImage image){
         super(image.getWidth(), image.getHeight(), image.getType());
         Graphics2D g = this.createGraphics();
         g.drawImage(image, 0, 0, null);
         g.dispose();
     }
-
-//    public Sprite(Sprite sprite){
-//        super(sprite.getWidth(), sprite.getHeight(), sprite.getType());
-//    }
 }
